420e25d40dd8c822524825ad3778b249ec9d00bb
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2026-02-03 10:49:57 -0300

    first commit

---
+++ security/src/test/java/dev/estudo/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.estudo.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
